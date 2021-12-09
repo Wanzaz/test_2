09e6e396311ccb26271c78eb9780f819f2f26ebb
Wanzaz <ondra.pazourek@gmail.com>
2021-12-09 20:01:16 +0100

    NSD modified to function

--- examples/16_NSD.c
+++ examples/16_NSD.c
@@ -3,32 +3,38 @@
 
 /*16. Napište funkci nsd, která pro dvě celá čísla vrátí jejich největší společný dělitel.*/
 
- int main ()
- {
-   int a, b;
-   printf ("Program určí největšího společného dělitele pro 2 zadaná čísla\n \n");
-   
-   printf ("\n Enter the first number:");
-   scanf ("%d", & a);
-   
-   printf ("\n Enter second number:");
-   scanf ("%d", & b);
-   
+int NSD(int a, int b)
+{
    if ((a == 0) || (b == 0))
-   {
-      printf ("Nelze určit, zda je jedno z čísel nula \n");
-      return 0;
-   }
-   if (a <0) a = -a;
-   if (b <0) b = -b;
-  
-   while (a != b)
-   {
-      if (a> b) a = a-b;
-           else b = b-a;
-   }
+  {
+     printf ("Cannot determine if one of the numbers is zero \n");
+     return 0;
+  }
+   
+  if (a <0) a = -a;
+  if (b <0) b = -b;
 
-   printf ("\n Největší společný dělitel je %d \n \n", a);
-
-   return 0;
+  while (a != b)
+  {
+     if (a> b) a = a-b;
+          else b = b-a;
+  }
+  return a;
  }
+
+
+int main ()
+{
+  int a, b;
+  printf ("The program will specify the largest common divisor for 2 specified numbers \n \n");
+
+  printf ("\n Enter the first number:");
+  scanf ("%d", & a);
+
+  printf ("\n Enter the second number:");
+  scanf ("%d", & b);
+
+
+  printf ("\n The largest common divisor is %d \n \n", NSD(a, b));
+  return 0;
+}
